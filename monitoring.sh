59975a7457e065b3c293046fbd502dc55112d30e
Ayoub Adouay <ayadouay@e2r10p9.1337.ma>
2024-12-12 16:24:40 +0100

    addiig

---
+++ monitoring.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+architecture=$(uname -a  | awk '{print $1 " " $2 " "  $3 " "  $4 " " $5 " " $7 " " $8 " " $9 " " $10 " " $11}')
+cpu=$(lscpu |grep "^CPU(s):" | awk '{print $2}')
+vcpu=$(lscpu |grep "^Thread(s) per core:" | awk '{print $4}')
+used_rm=$(free -m | grep "Mem" | awk '{print $3}')
+total_rm=$(free -m | grep "Mem" | awk '{print $2}')
+used_by_precentage=$(free -m | grep "Mem" | awk '{printf("%.2f", ($3/$2)*100)}')
+Diskused=$(df -H --total | grep "total" | awk  '{print $3}')
+Disktotal=$(df -H --total | grep "total" | awk '{print $2}')
+Dusage_percentage=$(df -H --total | grep "total" | awk  '{printf("%.f", ($3/$2)*100)}')
+cpu_load=$(top -bn2 | grep "%Cpu(s)" | awk 'NR==2'| awk '{print $2 "%"}')
+last_boot=$(who -b | awk '{print $3 " " $4}')
+check_lvm=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
+
+wall "
+	#Architecture : $architecture
+	#CPU physical : $cpu
+	#vCPU : $vcpu
+	#Memory Usage : $used_rm/$total_rm MB ($used_by_precentage%)
+	#Disk Usage : $Diskused/$Disktotal GB ($Dusage_percentage%)
+	#CPU Load : $cpu_load
+	#Last boot : $last_boot
+	#LVM use : $check_lvm
+"
